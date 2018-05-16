f945ed1ec7205839066f13e38aa242d9c0da9730
linzhijun <zhijun0223@126.com>
2018-05-16 16:35:07 +0800

    登录加上授权功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,5 @@
 import WxValidate from '../../static/js/plugin/WxValidate'
+import form from '../../static/js/plugin/form'
 const app = getApp();
 Page({
   data: {
@@ -16,7 +17,7 @@ Page({
     this.setData({
       submiting:true
     });
-
+    
     //表单验证
     if (!this.WxValidate.checkForm(e)) {
       const error = this.WxValidate.errorList[0]
@@ -27,60 +28,57 @@ Page({
       return false
     } else {
       //验证通过进行后端请求
-      
-      wx.request({
-        url: app.globalData.server +"/welcome/wechatapp?callback=Jiaju.check_login",
-        data: params,
-        method: 'post',
-        header: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        },
-        dataType: "json",
-        success: function (res) {
-          console.dir(res);
-          //设置提交按钮状态
-          that.setData({
-            submiting: false
+      that.form.requestPost(that.form.API_CONFIG['login'], params,function(res,header){
+        //设置提交按钮状态
+        that.setData({
+          submiting: false
+        });
+ 
+        //如果用户不存在或则错误
+        if (res.status != '1') {
+          that.setData({validateMsg: res.msg})
+        } else {
+          //动态全局赋值
+          app.globalData.sessionJdbUkey     = res.data["ukey"];
+          app.globalData.sessionJdbBrandId  = res.data["brand_id"];
+          app.globalData.sessionJdbUserInfo = res.data;
+
+          wx.login({
+            success:function(res){
+              if(res.code){
+                that.form.requestPost(that.form.API_CONFIG['bind'], { code: res.code, ukey: app.globalData.sessionJdbUkey, bid: app.globalData.sessionJdbBrandId}, function (res) {
+                  if(res.status == '1'){
+                    //本地存储id
+                    wx.setStorage({
+                      key: "sessionJdbUkey",
+                      data: app.globalData.sessionJdbUkey
+                    });
+
+                    wx.setStorage({
+                      key: "sessionJdbBrandId",
+                      data: app.globalData.sessionJdbBrandId
+                    });
+
+                    wx.setStorage({
+                      key: "sessionJdbUserInfo",
+                      data: app.globalData.sessionJdbUserInfo
+                    });
+
+                    //跳转到相关页面
+                    wx.redirectTo({
+                      url: '/pages/index/index'
+                    })
+                  }else{
+                    that.setData({ validateMsg: res.msg })
+                  }
+                });
+              }else{
+                that.setData({ validateMsg: '授权失败' })
+              }
+            }
           });
-          //如果用户不存在或则错误
-          if(res.data.status=="-1"){
-            that.setData({
-              validateMsg: res.data.msg
-            })
-          }else{
-            //用户存在
-            
-
-
-            //动态全局赋值
-            app.globalData.sessionJdbUkey = res.data.data["ukey"];
-            app.globalData.sessionJdbBrandId = res.data.data["brand_id"];
-            app.globalData.sessionJdbUserInfo = res.data.data;
-            
-            //本地存储id
-            wx.setStorage({
-              key: "sessionJdbUkey",
-              data: res.data.data["ukey"]
-            });
-
-            wx.setStorage({
-              key: "sessionJdbBrandId",
-              data: res.data.data["brand_id"]
-            });
-
-            wx.setStorage({
-              key: "sessionJdbUserInfo",
-              data: res.data.data
-            });
-
-            //跳转到相关页面
-            wx.redirectTo({
-              url: '/pages/index/index'
-            })
-          }
-         
         }
-      });
+      })
     }
   },
   //账号输入框事件
@@ -110,5 +108,7 @@ Page({
     }
 
     this.WxValidate = new WxValidate(rules, messages);
+    this.form = new form(app); 
+
   }
 })
