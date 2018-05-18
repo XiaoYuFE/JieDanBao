bf32935458ffeb6c7b48502a84c043a21d6f2b8b
efri-yang <948061564@qq.com>
2018-05-18 14:25:02 +0800

    c

--- pages/list/list.js
+++ pages/list/list.js
@@ -5,57 +5,112 @@ const app = getApp();
 Page({
   data: {
     toView: "",
-    step:"",
-    dataList: ""
+    step: "",
+    loadMoreData:"加载更多...",
+    scrollHeight:"",
+    dataList: "",
+    stepCurrObj:"",
+
+    stepall: {
+      currentPage: 1,
+      isLast:false,
+      list: []
+    },
+    step0: {
+      currentPage: 1,
+      isLast: false,
+      list: []
+    },
+    step1: {
+      currentPage: 1,
+      isLast: false,
+      list: []
+    },
+    step2: {
+      currentPage: 1,
+      isLast: false,
+      list: []
+    },
+    step3: {
+      currentPage: 1,
+      isLast: false,
+      list: []
+    },
+    step4: {
+      currentPage: 1,
+      isLast: false,
+      list: []
+    },
+    step6: {
+      currentPage: 1,
+      isLast: false,
+      list: []
+    },
+    stepsx: {
+      currentPage: 1,
+      isLast: false,
+      list: []
+    }
+
   },
 
   onLoad: function (options) {
+    console.dir(options);
+    var that = this;  
     this.setData({
-      step:options.type,
+      step: options.type,
       toView: options.toView
     });
-    this._getDataList(this.data.step);
+   
+    wx.getSystemInfo({
+      success: function (res) {
+        console.info(res.windowHeight);
+        that.setData({
+          scrollHeight: res.windowHeight
+        });
+      }
+    });  
+    // this._getDataList(this.data.step, this.data.dataAll[options.toView]);
   },
   navTap: function (event) {
     console.dir(event);
     var step;
-    var id=event.currentTarget.id;
-    if (id =="stepall"){
-      step="";
-    } else if (id == "step0"){
-      step=0;
+    var id = event.currentTarget.id;
+    if (id == "stepall") {
+      step = "";
+    } else if (id == "step0") {
+      step = 0;
     } else if (id == "step1") {
       step = 1;
     } else if (id == "step2") {
       step = 2;
     } else if (id == "step3") {
-      step =3;
+      step = 3;
     } else if (id == "step4") {
-      step =4;
+      step = 4;
     } else if (id == "step6") {
-      step =6;
-    } else if (id =="stepsx"){
-      step =-1;
+      step = 6;
+    } else if (id == "stepsx") {
+      step = -1;
     }
     this.setData({
-      step:step,
+      step: step,
       toView: event.currentTarget.id
     });
     //服务端请求数据(发送用户id和订单的类型)
-    this._getDataList(step);
+    this._getDataList(step, id);
   },
   onShow: function () {
-    this._getDataList(this.data.step);
+    this._getDataList(this.data.step, this.data[this.data.toView]);
   },
-  _getDataList: function (listType) {
-    console.dir(listType);
-    var that=this;
-    console.dir(app.globalData.sessionJdbBrandId);
-    console.dir(app.globalData.sessionJdbUkey);
+  _getDataList: function (listType, obj) {
+    var that = this;
     wx.request({
-      url: app.globalData.server + "/welcome/wechatapp?callback=Jiaju.dlist",
+      // url: app.globalData.server + "/welcome/wechatapp?callback=Jiaju.dlist",
+      url: "https://wnworld.com/api/JieDanBang/list01.php",
       data: {
-        step:listType,
+        page: obj.currentPage,
+        step: listType,
         bid: app.globalData.sessionJdbBrandId,
         ukey: app.globalData.sessionJdbUkey
       },
@@ -66,13 +121,30 @@ Page({
       dataType: "json",
       success: function (res) {
         console.dir(res.data.data.info);
+        if (!res.data.data.info || !res.data.data.info.length){
+          obj.isLast=true;
+        }
+        obj.currentPage++;
+        obj.list = obj.list.concat(res.data.data.info);
+        console.dir(obj);
         that.setData({
-          dataList: res.data.data.info
+          stepCurrObj: obj,
+          dataList: obj.list
         })
 
-       
+
       }
     })
+  },
+  lower:function(){
+    if (this.data.isLast) {
+      this.setData({
+        loadMoreData: '已经到顶'
+      });
+      return;
+    } else {
+      //  this._getDataList(this.data.step, this.data[this.data.toView]);
+    }
   }
 
 
